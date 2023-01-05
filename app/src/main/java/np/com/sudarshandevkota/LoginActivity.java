099ba89164e39571b15c1244674efb8b25b62110
Sudarshan <sudarshandevkota22@gmail.com>
2023-01-05 18:27:44 +0545

    login activity ui changed, progressbar added for checking the logged in state

--- app/src/main/java/np/com/sudarshandevkota/LoginActivity.java
+++ app/src/main/java/np/com/sudarshandevkota/LoginActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import np.com.sudarshandevkota.retrofit.ApiCalls;
@@ -21,6 +22,7 @@ import retrofit2.Response;
 public class LoginActivity extends AppCompatActivity {
     EditText emailET,passwordET;
     Button loginBtn,toSignUpBtn;
+    ProgressBar progressBar;
 
 
     @Override
@@ -31,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
         passwordET = findViewById(R.id.et_password);
         loginBtn = findViewById(R.id.btn_login);
         toSignUpBtn = findViewById(R.id.btn_toSignup);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        loginBtn.setEnabled(false);
         loginBtn.setOnClickListener(loginListener);
         toSignUpBtn.setOnClickListener(view -> startActivity(new Intent(view.getContext(),SignupActivity.class)));
         isLoggedIn();
@@ -51,6 +56,8 @@ public class LoginActivity extends AppCompatActivity {
 
     };
     private void login(String username,String password){
+        progressBar.setVisibility(View.VISIBLE);
+        loginBtn.setEnabled(false);
         ApiCalls api = RetrofitClient.getInstance().create(ApiCalls.class);
         Call<String> call = api.login(username,password);
         call.enqueue(new Callback<String>() {
@@ -66,11 +73,13 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<String> call, Throwable t) {
-                showToast("Login Failed");
+                showToast();
                 SharedPreferences preferences = getSharedPreferences("BudgetOn",MODE_PRIVATE);
                 SharedPreferences.Editor editor = preferences.edit();
                 editor.clear();
                 editor.apply();
+                loginBtn.setEnabled(true);
+                progressBar.setVisibility(View.INVISIBLE);
 
             }
         });
@@ -78,13 +87,16 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
-    private void showToast(String r){
-        Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
+    private void showToast(){
+        Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
     }
     private void isLoggedIn(){
         SharedPreferences preferences = getSharedPreferences("BudgetOn",MODE_PRIVATE);
         if(preferences.contains("username")){
             login(preferences.getString("username",""),preferences.getString("password",""));
+        }else{
+            loginBtn.setEnabled(true);
+            progressBar.setVisibility(View.INVISIBLE);
         }
 
     }
